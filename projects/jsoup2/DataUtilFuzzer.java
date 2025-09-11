7ead5609580a91d42c1ad15c609c96f3ef81f749
D.M.S <65443888+gonzo-code@users.noreply.github.com>
2025-09-11 11:09:29 -0400

    Handle missing DataUtil corpus binaries

---
+++ projects/jsoup2/DataUtilFuzzer.java
@@ -0,0 +1,32 @@
+package org.jsoup.fuzz;
+
+import com.code_intelligence.jazzer.api.FuzzedDataProvider;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import org.jsoup.helper.DataUtil;
+import org.jsoup.nodes.Document;
+
+public class DataUtilFuzzer {
+    private static final String[] CHARSETS = {
+        "UTF-8", "UTF-16", "UTF-16LE", "UTF-16BE", "UTF-32",
+        "ISO-8859-1", "windows-1252", "Shift_JIS", "KOI8-R", "Big5", null
+    };
+
+    public static void fuzzerTestOneInput(FuzzedDataProvider data) throws Throwable {
+      String baseUri = "http://" + data.consumeAsciiString(20);
+      if (baseUri.equals("http://")) {
+        baseUri += "example.com/";
+      }
+      String charset = data.pickValue(CHARSETS);
+      byte[] bytes = data.consumeRemainingAsBytes();
+      try (InputStream in = new ByteArrayInputStream(bytes)) {
+        Document doc = DataUtil.load(in, baseUri, charset);
+        if (doc != null) {
+          doc.outerHtml();
+        }
+      } catch (IllegalArgumentException | IOException e) {
+        // Known exception types.
+      }
+    }
+  }
